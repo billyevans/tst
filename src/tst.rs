f082edb91a18308f1f9539783e7f7d0aacebc7a6
billyevans <pervushinai@gmail.com>
2015-02-25 20:15:03 +0100

    Add is_empty(), contains_key() methods
    Replace tabs to spaces

--- src/tst.rs
+++ src/tst.rs
@@ -1,126 +1,126 @@
 /*
 
 public class TST<Value> {
-	private int N;       // size
-	private Node root;   // root of TST
+    private int N;       // size
+    private Node root;   // root of TST
 
-	private class Node {
-		private char c;                 // character
-		private Node left, mid, right;  // left, middle, and right subtries
-		private Value val;              // value associated with string
-	}
+    private class Node {
+        private char c;                 // character
+        private Node left, mid, right;  // left, middle, and right subtries
+        private Value val;              // value associated with string
+    }
 
-	// return number of key-value pairs
-	public int size() {
-		return N;
-	}
+    // return number of key-value pairs
+    public int size() {
+        return N;
+    }
 
-	public boolean contains(String key) {
-		return get(key) != null;
-	}
+    public boolean contains(String key) {
+        return get(key) != null;
+    }
 
-	public Value get(String key) {
-		if (key == null) throw new NullPointerException();
-		if (key.length() == 0) throw new IllegalArgumentException("key must have length >= 1");
-		Node x = get(root, key, 0);
-		if (x == null) return null;
-		return (Value) x.val;
-	}
+    public Value get(String key) {
+        if (key == null) throw new NullPointerException();
+        if (key.length() == 0) throw new IllegalArgumentException("key must have length >= 1");
+        Node x = get(root, key, 0);
+        if (x == null) return null;
+        return (Value) x.val;
+    }
 
-	// return subtrie corresponding to given key
-	private Node get(Node x, String key, int d) {
-		if (key == null) throw new NullPointerException();
-		if (key.length() == 0) throw new IllegalArgumentException("key must have length >= 1");
-		if (x == null) return null;
-		char c = key.charAt(d);
-		if      (c < x.c)              return get(x.left,  key, d);
-		else if (c > x.c)              return get(x.right, key, d);
-		else if (d < key.length() - 1) return get(x.mid,   key, d+1);
-		else                           return x;
-	}
-
-
-	public void put(String s, Value val) {
-		if (!contains(s)) N++;
-		root = put(root, s, val, 0);
-	}
-
-	private Node put(Node x, String s, Value val, int d) {
-		char c = s.charAt(d);
-		if (x == null) {
-			x = new Node();
-			x.c = c;
-		}
-		if      (c < x.c)             x.left  = put(x.left,  s, val, d);
-		else if (c > x.c)             x.right = put(x.right, s, val, d);
-		else if (d < s.length() - 1)  x.mid   = put(x.mid,   s, val, d+1);
-		else                          x.val   = val;
-		return x;
-	}
+    // return subtrie corresponding to given key
+    private Node get(Node x, String key, int d) {
+        if (key == null) throw new NullPointerException();
+        if (key.length() == 0) throw new IllegalArgumentException("key must have length >= 1");
+        if (x == null) return null;
+        char c = key.charAt(d);
+        if      (c < x.c)              return get(x.left,  key, d);
+        else if (c > x.c)              return get(x.right, key, d);
+        else if (d < key.length() - 1) return get(x.mid,   key, d+1);
+        else                           return x;
+    }
 
 
-	public String longestPrefixOf(String s) {
-		if (s == null || s.length() == 0) return null;
-		int length = 0;
-		Node x = root;
-		int i = 0;
-		while (x != null && i < s.length()) {
-			char c = s.charAt(i);
-			if      (c < x.c) x = x.left;
-			else if (c > x.c) x = x.right;
-			else {
-				i++;
-				if (x.val != null) length = i;
-				x = x.mid;
-			}
-		}
-		return s.substring(0, length);
-	}
+    public void put(String s, Value val) {
+        if (!contains(s)) N++;
+        root = put(root, s, val, 0);
+    }
 
-	// all keys in symbol table
-	public Iterable<String> keys() {
-		Queue<String> queue = new Queue<String>();
-		collect(root, "", queue);
-		return queue;
-	}
-
-	// all keys starting with given prefix
-	public Iterable<String> prefixMatch(String prefix) {
-		Queue<String> queue = new Queue<String>();
-		Node x = get(root, prefix, 0);
-		if (x == null) return queue;
-		if (x.val != null) queue.enqueue(prefix);
-		collect(x.mid, prefix, queue);
-		return queue;
-	}
-
-	// all keys in subtrie rooted at x with given prefix
-	private void collect(Node x, String prefix, Queue<String> queue) {
-		if (x == null) return;
-		collect(x.left,  prefix,       queue);
-		if (x.val != null) queue.enqueue(prefix + x.c);
-		collect(x.mid,   prefix + x.c, queue);
-		collect(x.right, prefix,       queue);
-	}
+    private Node put(Node x, String s, Value val, int d) {
+        char c = s.charAt(d);
+        if (x == null) {
+            x = new Node();
+            x.c = c;
+        }
+        if      (c < x.c)             x.left  = put(x.left,  s, val, d);
+        else if (c > x.c)             x.right = put(x.right, s, val, d);
+        else if (d < s.length() - 1)  x.mid   = put(x.mid,   s, val, d+1);
+        else                          x.val   = val;
+        return x;
+    }
 
 
-	// return all keys matching given wilcard pattern
-	public Iterable<String> wildcardMatch(String pat) {
-		Queue<String> queue = new Queue<String>();
-		collect(root, "", 0, pat, queue);
-		return queue;
-	}
+    public String longestPrefixOf(String s) {
+        if (s == null || s.length() == 0) return null;
+        int length = 0;
+        Node x = root;
+        int i = 0;
+        while (x != null && i < s.length()) {
+            char c = s.charAt(i);
+            if      (c < x.c) x = x.left;
+            else if (c > x.c) x = x.right;
+            else {
+                i++;
+                if (x.val != null) length = i;
+                x = x.mid;
+            }
+        }
+        return s.substring(0, length);
+    }
+
+    // all keys in symbol table
+    public Iterable<String> keys() {
+        Queue<String> queue = new Queue<String>();
+        collect(root, "", queue);
+        return queue;
+    }
+
+    // all keys starting with given prefix
+    public Iterable<String> prefixMatch(String prefix) {
+        Queue<String> queue = new Queue<String>();
+        Node x = get(root, prefix, 0);
+        if (x == null) return queue;
+        if (x.val != null) queue.enqueue(prefix);
+        collect(x.mid, prefix, queue);
+        return queue;
+    }
+
+    // all keys in subtrie rooted at x with given prefix
+    private void collect(Node x, String prefix, Queue<String> queue) {
+        if (x == null) return;
+        collect(x.left,  prefix,       queue);
+        if (x.val != null) queue.enqueue(prefix + x.c);
+        collect(x.mid,   prefix + x.c, queue);
+        collect(x.right, prefix,       queue);
+    }
+
+
+    // return all keys matching given wilcard pattern
+    public Iterable<String> wildcardMatch(String pat) {
+        Queue<String> queue = new Queue<String>();
+        collect(root, "", 0, pat, queue);
+        return queue;
+    }
  
-	public void collect(Node x, String prefix, int i, String pat, Queue<String> q) {
-		if (x == null) return;
-		char c = pat.charAt(i);
-		if (c == '.' || c < x.c) collect(x.left, prefix, i, pat, q);
-		if (c == '.' || c == x.c) {
-			if (i == pat.length() - 1 && x.val != null) q.enqueue(prefix + x.c);
-			if (i < pat.length() - 1) collect(x.mid, prefix + x.c, i+1, pat, q);
-		}
-		if (c == '.' || c > x.c) collect(x.right, prefix, i, pat, q);
-	}
+    public void collect(Node x, String prefix, int i, String pat, Queue<String> q) {
+        if (x == null) return;
+        char c = pat.charAt(i);
+        if (c == '.' || c < x.c) collect(x.left, prefix, i, pat, q);
+        if (c == '.' || c == x.c) {
+            if (i == pat.length() - 1 && x.val != null) q.enqueue(prefix + x.c);
+            if (i < pat.length() - 1) collect(x.mid, prefix + x.c, i+1, pat, q);
+        }
+        if (c == '.' || c > x.c) collect(x.right, prefix, i, pat, q);
+    }
 }*/
 
 
@@ -134,92 +134,98 @@ public class TST<Value> {
 #[allow(dead_code)]
 
 use std::ptr;
-use std::mem;
+use std::boxed;
 
 struct Node<Value> {
-	left: *mut Node<Value>,
-	mid: *mut Node<Value>,
-	right: *mut Node<Value>,
-	val: *mut Value,
-	c: char
+    left: *mut Node<Value>,
+    mid: *mut Node<Value>,
+    right: *mut Node<Value>,
+    val: *mut Value,
+    c: char
 }
 
 impl<Value> Node<Value> {
-	fn new() -> Node<Value> {
-		Node {
-			left : ptr::null_mut(),
-			mid : ptr::null_mut(),
-			right : ptr::null_mut(),
-			val : ptr::null_mut(),
-			c : '\0'
-		}
-	}
+    fn new() -> Node<Value> {
+        Node {
+            left : ptr::null_mut(),
+            mid : ptr::null_mut(),
+            right : ptr::null_mut(),
+            val : ptr::null_mut(),
+            c : '\0'
+        }
+    }
 }
 
 pub struct TST<Value: Clone> {
-	root: *mut Node<Value>,
-	size: usize
+    root: *mut Node<Value>,
+    size: usize
 }
 
 impl<Value: Clone> TST<Value> {
-	pub fn new() -> TST<Value> {
-		TST { root : ptr::null_mut(), size : 0 }
-	}
+    pub fn new() -> TST<Value> {
+        TST { root : ptr::null_mut(), size : 0 }
+    }
 
-	pub fn size(&self) -> usize { self.size }
+    pub fn len(&self) -> usize { self.size }
 
-	pub fn put(&mut self, key: &str, val: &Value) {
-		unsafe { self.root = self.put_(self.root, key.chars().collect(), val, 0); }
-	}
+    pub fn insert(&mut self, key: &str, val: &Value) {
+        unsafe { self.root = self.insert_(self.root, key.chars().collect(), val, 0); }
+    }
 
-	unsafe fn put_(&mut self, mut x: *mut Node<Value>, key: Vec<char>, val: &Value, d: usize) -> *mut Node<Value> {
-		let c = key[d];
-		if x.is_null() {
-			x = mem::transmute(Box::new(Node::<Value>::new()));
-			(*x).c = c;
-		}
-		if c < (*x).c {
-			(*x).left  = self.put_((*x).left, key, val, d);
-		}
-		else if c > (*x).c {
-			(*x).right = self.put_((*x).right, key, val, d);
-		}
-		else if d+1 < key.len() {
-			(*x).mid = self.put_((*x).mid, key, val, d+1);
-		}
-		else {
-			if (*x).val.is_null() {
-				self.size += 1;
-			}
-			(*x).val   = mem::transmute(Box::new(val.clone()));
-		}
-		return x;
-	}
+    unsafe fn insert_(&mut self, mut x: *mut Node<Value>, key: Vec<char>, val: &Value, d: usize) -> *mut Node<Value> {
+        let c = key[d];
+        if x.is_null() {
+            x = boxed::into_raw(Box::new(Node::<Value>::new()));
+            (*x).c = c;
+        }
+        if c < (*x).c {
+            (*x).left  = self.insert_((*x).left, key, val, d);
+        }
+        else if c > (*x).c {
+            (*x).right = self.insert_((*x).right, key, val, d);
+        }
+        else if d+1 < key.len() {
+            (*x).mid = self.insert_((*x).mid, key, val, d+1);
+        }
+        else {
+            if (*x).val.is_null() {
+                self.size += 1;
+            }
+            (*x).val = boxed::into_raw(Box::new(val.clone()));
+        }
+        return x;
+    }
 
-	pub fn get(&self, key: &str) -> Option<&Value> {
-		unsafe {
-			match self.get_(self.root, key.chars().collect(), 0) {
-				Some(x) => Some(&(*x.val)),
-				None => None
-			}
-		}
-	}
-	// return subtrie corresponding to given key
-	unsafe fn get_(&self, x: *mut Node<Value>, key: Vec<char>, d: usize) -> Option<&Node<Value> > {
-		if x.is_null() { return None; }
-		if d >= key.len() { return None; }
-		let c = key[d];
-		if c < (*x).c {
-			return self.get_((*x).left, key, d);
-		}
-		else if c > (*x).c {
-			return self.get_((*x).right, key, d);
-		}
-		else if d + 1< key.len() {
-			return self.get_((*x).mid, key, d+1);
-		}
-		else {
-			return Some(&(*x));
-		}
-	} 
+    pub fn get(&self, key: &str) -> Option<&Value> {
+        unsafe {
+            match self.get_(self.root, key.chars().collect(), 0) {
+                Some(x) => Some(&(*x.val)),
+                None => None
+            }
+        }
+    }
+    // return subtrie corresponding to given key
+    unsafe fn get_(&self, x: *mut Node<Value>, key: Vec<char>, d: usize) -> Option<&Node<Value> > {
+        if x.is_null() { return None; }
+        if d >= key.len() { return None; }
+        let c = key[d];
+        if c < (*x).c {
+            return self.get_((*x).left, key, d);
+        }
+        else if c > (*x).c {
+            return self.get_((*x).right, key, d);
+        }
+        else if d + 1< key.len() {
+            return self.get_((*x).mid, key, d+1);
+        }
+        else {
+            return Some(&(*x));
+        }
+    }
+    pub fn contains_key(&self, key: &str) -> bool {
+        !self.get(key).is_none()
+    }
+    pub fn is_empty(&self) -> bool {
+        self.size == 0
+    }
 }
--- tests/ut.rs
+++ tests/ut.rs
@@ -4,57 +4,80 @@ extern crate tst;
 use tst::tst::*;
 #[test]
 fn create_root() {
-	let m = TST::<i32>::new();
-	assert_eq!(0, m.size());
+    let m = TST::<i32>::new();
+    assert_eq!(0, m.len());
 }
 
 #[test]
-fn put() {
-	let mut m = TST::<i32>::new();
+fn insert() {
+    let mut m = TST::<i32>::new();
 
-	m.put("abc", &13);
-	assert_eq!(1, m.size());
+    m.insert("abc", &13);
+    assert_eq!(1, m.len());
 }
 
 #[test]
 fn get() {
-	let mut m = TST::<i32>::new();
+    let mut m = TST::<i32>::new();
 
-	m.put("abc", &13);
-	assert_eq!(Some(&13), m.get("abc"));
+    m.insert("abc", &13);
+    assert_eq!(Some(&13), m.get("abc"));
 }
 
 #[test]
 fn get_none() {
-	let mut m = TST::<i32>::new();
+    let mut m = TST::<i32>::new();
 
-	m.put("abc", &13);
-	assert_eq!(None, m.get("abcd"));
-	assert_eq!(None, m.get(""));
+    m.insert("abc", &13);
+    assert_eq!(None, m.get("abcd"));
+    assert_eq!(None, m.get(""));
 }
 
 #[test]
-fn put_few() {
-	let mut m = TST::<i32>::new();
+fn insert_few() {
+    let mut m = TST::<i32>::new();
 
-	m.put("abcde", &13);
-	m.put("abcdf", &14);
-	m.put("abcdg", &15);
-	assert_eq!(3, m.size());
+    m.insert("abcde", &13);
+    m.insert("abcdf", &14);
+    m.insert("abcdg", &15);
+    assert_eq!(3, m.len());
 
-	assert_eq!(Some(&13), m.get("abcde"));
-	assert_eq!(Some(&14), m.get("abcdf"));
-	assert_eq!(Some(&15), m.get("abcdg"));
-	assert_eq!(None, m.get("abcdh"));
+    assert_eq!(Some(&13), m.get("abcde"));
+    assert_eq!(Some(&14), m.get("abcdf"));
+    assert_eq!(Some(&15), m.get("abcdg"));
+    assert_eq!(None, m.get("abcdh"));
 }
 
 #[test]
 fn replace() {
-	let mut m = TST::<i32>::new();
+    let mut m = TST::<i32>::new();
 
-	m.put("abcde", &13);
-	m.put("abcde", &1);
-	assert_eq!(1, m.size());
+    m.insert("abcde", &13);
+    m.insert("abcde", &1);
+    assert_eq!(1, m.len());
 
-	assert_eq!(Some(&1), m.get("abcde"));
+    assert_eq!(Some(&1), m.get("abcde"));
 }
+
+#[test]
+fn contains() {
+    let mut m = TST::<i32>::new();
+
+    m.insert("xxxe", &13);
+    assert!(!m.contains_key("abcde"));
+    assert!(!m.contains_key("xxx"));
+    assert!(m.contains_key("xxxe"));
+}
+
+#[test]
+fn is_empty() {
+    let mut m = TST::<u32>::new();
+
+    assert_eq!(0, m.len());
+    assert!(m.is_empty());
+
+    m.insert("qwer", &10000);
+    assert!(!m.is_empty());
+    // TODO: add clear() and check again
+}
+
