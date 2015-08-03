4aeb541d98efb9d73944eb08ab590b961bb5cb9c
billyevans <pervushinai@gmail.com>
2015-08-03 19:02:15 +0200

    exclude gh-pages from travis, add addons section to .travis.yml

--- install-kcov.sh
+++ install-kcov.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 set -e
-apt-get install libcurl4-openssl-dev libelf-dev libdw-dev
 wget https://github.com/SimonKagstrom/kcov/archive/master.tar.gz
-tar xzf master.tar.gz 
-mkdir kcov-master/build 
+tar xzf master.tar.gz
+mkdir kcov-master/build
 cd kcov-master/build && cmake .. && make && make install && make install
