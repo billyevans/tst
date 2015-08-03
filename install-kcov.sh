3951cff85f61247c54d7911f090a683b790a0c5d
billyevans <pervushinai@gmail.com>
2015-08-03 23:30:11 +0200

    Add prefix to install-kcov.sh

--- install-kcov.sh
+++ install-kcov.sh
@@ -3,4 +3,4 @@ set -e
 wget https://github.com/SimonKagstrom/kcov/archive/master.tar.gz
 tar xzf master.tar.gz
 mkdir kcov-master/build
-cd kcov-master/build && cmake .. && make && make install && make install
+cd kcov-master/build && cmake -DCMAKE_INSTALL_PREFIX:PATH=$HOME/kcov .. && make && make install && make install
