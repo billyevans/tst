dbda943757915aa57d43c5cabc207d518684722b
docbot@travis <doc bot>
2015-09-04 14:48:12 +0000

    rebuild pages at 9160996

--- tst/sidebar-items.js
+++ tst/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"macro":[["tstmap!","Create a `TST` containing a given list of elements:\n"]],"mod":[["tst",""]]});
+initSidebarItems({"macro":[["tstmap!","Create a `TST` containing a given list of elements:\n"]],"mod":[["tst","TST container map implementation."]]});
