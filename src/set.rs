97ac56b51471214c2c2b54062c6063339f14c79f
billyevans <pervushinai@gmail.com>
2016-01-06 23:46:47 +0000

    Add into_iter, wildcard_iter, prefix_iter, longetst_prefix to TSTSet
    Fixes #3

--- src/set.rs
+++ src/set.rs
@@ -1,5 +1,6 @@
-use tst_map::{TSTMap, KeysIter};
+use tst_map::{self, TSTMap};
 use std::fmt::{self, Debug};
+use std::iter::{Map, FromIterator};
 
 /// A set based on a TSTMap.
 #[derive(Clone, PartialEq, Eq)]
@@ -10,7 +11,18 @@ pub struct TSTSet {
 /// An iterator over a TSTSet's items.
 #[derive(Clone)]
 pub struct Iter<'a> {
-    iter: KeysIter<'a, ()>
+    iter: Map<tst_map::Iter<'a, ()>, fn((String, &'a ())) -> String>
+}
+
+/// An owning iterator over a TSTSet's items.
+pub struct IntoIter {
+    iter: Map<tst_map::IntoIter<()>, fn((String, ())) -> String>
+}
+
+/// TSTMap wild-card iterator.
+#[derive(Clone)]
+pub struct WildCardIter<'a> {
+    iter: Map<tst_map::WildCardIter<'a, ()>, fn( (String, &'a () )) -> String>,
 }
 
 impl TSTSet {
@@ -143,7 +155,124 @@ impl TSTSet {
     /// }
     /// ```
     pub fn iter(&self) -> Iter {
-        Iter { iter: self.map.keys() }
+        fn first<A, B>((a, _): (A, B)) -> A { a }
+        Iter { iter: self.map.iter().map(first) }
+
+        //Iter { iter: self.map.keys() }
+    }
+
+    /// An iterator returning all nodes matching wildcard pattern.
+    /// Iterator element type is (String)
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use tst::TSTSet;
+    ///
+    /// let mut s = TSTSet::new();
+    /// s.insert("a");
+    /// s.insert("b");
+    /// s.insert("c");
+    ///
+    /// for x in s.wildcard_iter(".") {
+    ///     println!("{}", x);
+    /// }
+    /// ```
+    pub fn wildcard_iter(&self, pat: &str) -> WildCardIter {
+        fn first<A, B>((a, _): (A, B)) -> A { a }
+        WildCardIter { iter: self.map.wildcard_iter(pat).map(first) }
+    }
+
+    /// Method returns longest prefix in the TSTSet.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use tst::TSTSet;
+    /// let mut set = TSTSet::new();
+    /// set.insert("abc");
+    /// set.insert("abcd");
+    /// set.insert("abce");
+    /// set.insert("abca");
+    /// set.insert("zxd");
+    /// set.insert("add");
+    /// set.insert("abcdef");
+    ///
+    /// assert_eq!("abcd", set.longest_prefix("abcde"));
+    /// ```
+    pub fn longest_prefix<'a>(&self, pref: &'a str) -> &'a str {
+        self.map.longest_prefix(pref)
+    }
+
+    /// Method returns iterator over all values with common prefix in the TSTSet.
+    /// # Examples
+    ///
+    /// ```
+    /// use tst::TSTSet;
+    /// let mut set = TSTSet::new();
+    /// set.insert("abc");
+    /// set.insert("abcd");
+    /// set.insert("abce");
+    /// set.insert("abca");
+    /// set.insert("zxd");
+    /// set.insert("add");
+    /// set.insert("abcdef");
+    ///
+    /// for key in set.prefix_iter("abc") {
+    ///     println!("{}", key);
+    /// }
+    ///
+    /// let first_key = set.iter().next().unwrap();
+    /// assert_eq!("abc".to_string(), first_key);
+    /// ```
+    pub fn prefix_iter(&self, pref: &str) -> Iter {
+        fn first<A, B>((a, _): (A, B)) -> A { a }
+        Iter { iter: self.map.prefix_iter(pref).map(first) }
+    }
+}
+
+impl IntoIterator for TSTSet {
+    type Item = String;
+    type IntoIter = IntoIter;
+
+    /// Creates a consuming iterator, that is, one that moves each key-value
+    /// pair out of the TSTMap in arbitrary order. The TSTMap cannot be used after
+    /// calling this.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use tst::TSTSet;
+    ///
+    /// let mut set = TSTSet::new();
+    /// set.insert("a");
+    /// set.insert("b");
+    /// set.insert("c");
+    ///
+    /// let vec: Vec<String> = set.into_iter().collect();
+    /// ```
+    fn into_iter(self) -> IntoIter {
+        fn first<A, B>((a, _): (A, B)) -> A { a }
+        IntoIter { iter: self.map.into_iter().map(first) }
+    }
+}
+
+impl<'x> FromIterator<&'x str> for TSTSet {
+    fn from_iter<I: IntoIterator<Item = &'x str>>(iter: I) -> TSTSet {
+        let mut set = TSTSet::new();
+        for item in iter {
+            set.insert(item);
+        }
+        set
+    }
+}
+
+impl<'x> Extend<(&'x str)> for TSTSet {
+    #[inline]
+    fn extend<I: IntoIterator<Item=(&'x str)>>(&mut self, iter: I) {
+        for k in iter {
+            self.insert(k);
+        }
     }
 }
 
@@ -158,6 +287,24 @@ impl Debug for TSTSet {
 }
 
 impl<'a> Iterator for Iter<'a> {
+    type Item = String;
+
+    fn next(&mut self) -> Option<String> { self.iter.next() }
+    fn size_hint(&self) -> (usize, Option<usize>) { self.iter.size_hint() }
+}
+
+impl Iterator for IntoIter {
+    type Item = String;
+
+    fn next(&mut self) -> Option<String> { self.iter.next() }
+    fn size_hint(&self) -> (usize, Option<usize>) { self.iter.size_hint() }
+}
+
+impl ExactSizeIterator for IntoIter {
+    fn len(&self) -> usize { self.iter.len() }
+}
+
+impl<'a> Iterator for WildCardIter<'a> {
     type Item = String;
 
     fn next(&mut self) -> Option<String> { self.iter.next() }
--- tests/set_ut.rs
+++ tests/set_ut.rs
@@ -1,11 +1,89 @@
 #[macro_use] extern crate tst;
 
 use self::tst::TSTSet;
+use std::iter::FromIterator;
+
+fn prepare_data() -> TSTSet {
+    tstset!(
+        "BY",
+        "BYGONE",
+        "BYE",
+        "BYLAW",
+        "BYLINE",
+        "BYPASS",
+        "BYPATH",
+        "BYPRODUCT",
+        "BYROAD",
+        "BYSTANDER",
+        "BYTE",
+        "BYWAY",
+        "BYWORD",
+    )
+}
 
 #[test]
 fn create_root() {
     let s = TSTSet::new();
     assert_eq!(0, s.len());
+}
+
+#[test]
+fn len() {
+    let mut s = TSTSet::new();
+
+    s.insert("abc");
+    s.insert("abd");
+    s.insert("xxx_xx");
+    s.insert("wow!");
+    s.insert("deadbeef");
+
+    assert_eq!(5, s.len());
+
+    s.remove("abc");
+    s.remove("abd");
+    assert_eq!(3, s.len());
+}
+
+#[test]
+fn is_empty_empty() {
+    let s = TSTSet::new();
+
+    assert_eq!(true, s.is_empty());
+}
+
+#[test]
+fn is_empty_false() {
+    let mut s = TSTSet::new();
+    s.insert("deadbeef");
+
+    assert_eq!(false, s.is_empty());
+}
+
+#[test]
+fn is_empty_true() {
+    let mut s = TSTSet::new();
+    s.insert("deadbeef");
+    s.clear();
+
+    assert_eq!(true, s.is_empty());
+}
+
+#[test]
+fn contains_empty() {
+    let s = TSTSet::new();
+
+    assert_eq!(false, s.contains("abc"));
+}
+
+#[test]
+fn contains() {
+    let mut s = TSTSet::new();
+
+    s.insert("abc");
+
+    assert_eq!(true, s.contains("abc"));
+    assert_eq!(false, s.contains("ab"));
+    assert_eq!(false, s.contains("abd"));
 }
 
 #[test]
@@ -23,4 +101,152 @@ fn insert_same() {
     assert_eq!(true, s.insert("abc"));
     assert_eq!(false, s.insert("abc"));
     assert_eq!(1, s.len());
+}
+
+#[test]
+fn remove_empty() {
+    let mut s = TSTSet::new();
+
+    assert_eq!(false, s.remove("abc"));
+}
+
+#[test]
+fn remove() {
+    let mut s = TSTSet::new();
+
+    s.insert("abc");
+    assert_eq!(true, s.remove("abc"));
+    assert_eq!(false, s.remove("abc"));
+    assert_eq!(true, s.is_empty());
+}
+
+#[test]
+fn iter() {
+    let set = tstset!("abc", "xxx", "qwer", "asdasd");
+    let expected: Vec<String> = vec![
+        "abc".to_string(),
+        "asdasd".to_string(),
+        "qwer".to_string(),
+        "xxx".to_string(),
+    ];
+
+    assert_eq!(expected, set.iter().collect::<Vec<String>>());
+}
+
+#[test]
+fn into_iter() {
+    let set = tstset!(
+        "b",
+        "a",
+        "c",
+        "aa",
+        "zzzzxxx"
+    );
+    let vec = set.into_iter().collect::<Vec<String>>();
+    let orig = vec![
+        "a".to_string(),
+        "aa".to_string(),
+        "b".to_string(),
+        "c".to_string(),
+        "zzzzxxx".to_string(),
+    ];
+    assert_eq!(orig, vec);
+}
+
+#[test]
+fn from_iterator_empty() {
+    let vec = vec![];
+    let set = TSTSet::from_iter(vec);
+
+    assert_eq!(true, set.is_empty());
+}
+
+#[test]
+fn from_iterator() {
+    let vec = vec![
+        "b",
+        "a",
+        "c",
+        "a",
+        "aa",
+    ];
+
+    let set = TSTSet::from_iter(vec);
+    let expected = tstset!(
+        "b",
+        "c",
+        "a",
+        "aa",
+    );
+    assert_eq!(expected, set);
+}
+
+#[test]
+fn extend() {
+    let mut set = tstset!("a");
+    let vec = vec![
+        "b",
+        "a",
+        "c",
+        "a",
+        "aa",
+    ];
+    set.extend(vec);
+    let expected = tstset!(
+        "a",
+        "b",
+        "c",
+        "aa",
+    );
+    assert_eq!(expected, set);
+}
+
+#[test]
+fn wild_card_iterator_simple() {
+    let s = tstset!("x", "y", "z");
+
+    assert_eq!("xyz", s.wildcard_iter(".").collect::<String>());
+}
+
+#[test]
+fn wild_card_iterator() {
+    let s = prepare_data();
+
+    assert_eq!("BYPASSBYPATH",  s.wildcard_iter("BYPA..").collect::<String>());
+}
+
+#[test]
+fn longest_prefix_empty() {
+    let set = tstset!("abc");
+
+    assert_eq!("", set.longest_prefix("qwer"));
+    assert_eq!("", set.longest_prefix(""));
+}
+
+#[test]
+fn longest_prefix() {
+    let set = tstset!(
+        "abc",
+        "abcd",
+        "abce",
+        "abca",
+        "zxd",
+        "add",
+        "abcdef",
+    );
+
+    assert_eq!("abcd", set.longest_prefix("abcde"));
+    assert_eq!("abcdef", set.longest_prefix("abcdef"));
+}
+
+#[test]
+fn prefix_iterator() {
+    let set = tstset!(
+        "first",
+        "second",
+        "firstthird",
+        "firstsecond",
+    );
+
+    assert_eq!("firstfirstsecondfirstthird",  set.prefix_iter("fir").collect::<String>());
 }
