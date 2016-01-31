0b242b343c62ace9fe38d29f31c121352f3908cc
billyevans <pervushinai@gmail.com>
2016-01-31 22:44:02 +0000

    Use TSTSet in dict.rs

--- examples/dict.rs
+++ examples/dict.rs
@@ -1,51 +1,51 @@
 extern crate tst;
 
 use std::env;
-use self::tst::TSTMap;
+use self::tst::TSTSet;
 use std::io;
 use std::fs::File;
 use std::io::prelude::*;
 
-
-fn match_prefix(m: &TSTMap<u8>, prefix: &str) {
+fn match_prefix(set: &TSTSet, prefix: &str) {
     println!("match('{}'):", prefix);
     if prefix.len() > 0 {
-        for (k, _) in m.prefix_iter(prefix) {
+        for k in set.prefix_iter(prefix) {
             println!("{}", k);
         }
-    }
-    else {
-        for (k, _) in m.iter() {
+    } else {
+        for k in set.iter() {
             println!("{}", k);
         }
     }
 }
 
-fn load_dict(path: &str, m: &mut TSTMap<u8>) -> io::Result<()> {
+fn load_dict(path: &str, set: &mut TSTSet) -> io::Result<()> {
     let mut buffer = String::new();
     let mut file = try!(File::open(path));
     try!(file.read_to_string(&mut buffer));
     // TODO: Add shuffle sort of lines, for better spread
     for line in buffer.split('\n') {
         if line.len() > 0 {
-            m.insert(line, 0);
+            set.insert(line);
         }
     }
-
     Ok(())
 }
 
 fn main() {
-    let mut m = TSTMap::<u8>::new();
-
     if env::args().count() < 3 {
-        panic!("usage: {} <dict>  <prefix1> [<prefix2> ...]", &env::args().nth(0).unwrap());
+        panic!("usage: {} <dict>  <prefix1> [<prefix2> ...]",
+               &env::args().nth(0).unwrap());
     }
     // read dict
-    load_dict(&env::args().nth(1).unwrap(), &mut m).unwrap();
+    let mut set = TSTSet::new();
+    load_dict(&env::args().nth(1).unwrap(), &mut set).unwrap();
 
     // print matched with prefix
-    for argument in env::args() {
-        match_prefix(&m, &argument);
+    let mut args = env::args();
+    args.next();
+    args.next();
+    for argument in args {
+        match_prefix(&set, &argument);
     }
 }
