c4c19a236de7fe6733672fa628b95046b265cc6a
billyevans <pervushinai@gmail.com>
2018-01-05 01:11:28 -0800

    Fix filename of kcov

--- install-kcov.sh
+++ install-kcov.sh
@@ -3,6 +3,6 @@ set -e
 KCOV_VERSION=34
 
 wget https://github.com/SimonKagstrom/kcov/archive/v${KCOV_VERSION}.tar.gz
-tar xzf master.tar.gz
+tar xzf v${KCOV_VERSION}.tar.gz
 mkdir kcov-${KCOV_VERSION}/build
 cd kcov-${KCOV_VERSION}/build && cmake -DCMAKE_INSTALL_PREFIX:PATH=$HOME/kcov .. && make && make install && make install
