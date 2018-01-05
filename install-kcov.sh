2006f8d2a8840ae29c45787869e75f2546557541
billyevans <pervushinai@gmail.com>
2018-01-05 01:08:07 -0800

    Pin version of kcov

--- install-kcov.sh
+++ install-kcov.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 set -e
-wget https://github.com/SimonKagstrom/kcov/archive/master.tar.gz
+KCOV_VERSION=34
+
+wget https://github.com/SimonKagstrom/kcov/archive/v${KCOV_VERSION}.tar.gz
 tar xzf master.tar.gz
-mkdir kcov-master/build
-cd kcov-master/build && cmake -DCMAKE_INSTALL_PREFIX:PATH=$HOME/kcov .. && make && make install && make install
+mkdir kcov-${KCOV_VERSION}/build
+cd kcov-${KCOV_VERSION}/build && cmake -DCMAKE_INSTALL_PREFIX:PATH=$HOME/kcov .. && make && make install && make install
