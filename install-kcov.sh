93b0278f2d22aa3ce992ce197946364d78133c24
billyevans <pervushinai@gmail.com>
2018-07-22 17:50:29 -0700

    Use docker elmtai/docker-rust-kcov for coverage
    Fixes #9

--- install-kcov.sh
+++ install-kcov.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 set -e
-KCOV_VERSION=34
+KCOV_VERSION=33
 
 wget https://github.com/SimonKagstrom/kcov/archive/v${KCOV_VERSION}.tar.gz
 tar xzf v${KCOV_VERSION}.tar.gz
 mkdir kcov-${KCOV_VERSION}/build
-cd kcov-${KCOV_VERSION}/build && cmake -DCMAKE_INSTALL_PREFIX:PATH=$HOME/kcov .. && make && make install && make install
+cd kcov-${KCOV_VERSION}/build && cmake -DCMAKE_INSTALL_PREFIX:PATH=$HOME/kcov .. && make && make install
